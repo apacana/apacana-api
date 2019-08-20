f6eacf5fd21de500aca47f1f4fff8589022855c8
lupengyu <729387121@qq.com>
2019-08-20 17:17:25 +0800

    fix: change allow origin
    Change-Id: Icd188bf153cf314d6b0cd39d804e6766bf8addc1

--- biz/middleware/cors.go
+++ biz/middleware/cors.go
@@ -1,14 +1,12 @@
 package middleware
 
 import (
-	"fmt"
 	"github.com/gin-gonic/gin"
 )
 
 func AddAllowOriginMW(c *gin.Context) {
 	method := c.Request.Method
 	origin := c.Request.Header.Get("Origin")
-	fmt.Println(method, origin)
 	if origin == "" {
 		return
 	}
@@ -18,7 +16,7 @@ func AddAllowOriginMW(c *gin.Context) {
 		c.Header("Access-Control-Allow-Headers", accessHeaders)
 	}
 
-	c.Header("Access-Control-Allow-Origin", "*")
+	c.Header("Access-Control-Allow-Origin", origin)
 	c.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, HEAD")
 	c.Header("Access-Control-Allow-Credentials", "true")
 	c.Header("Access-Control-Max-Age", "86400")
